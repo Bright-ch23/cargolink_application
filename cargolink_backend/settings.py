bfaaa13f1962f6f55d9ebcc378ed64ad22ca2868
Eng. Herman Bright <chehermannbright@gmail.com>
2026-04-18 02:42:32 +0100

    new commit for imports

--- cargolink_backend/settings.py
+++ cargolink_backend/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 import dj_database_url
@@ -6,6 +7,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(os.path.join(BASE_DIR))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-only-key')
 
